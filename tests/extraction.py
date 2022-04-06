173e5c45d135ac2eb42744c96ba4f821aaa109a8
Max Greenwood <71552700+TheRosemaryProject@users.noreply.github.com>
2022-04-06 11:41:29 +0100

    test files added

--- tests/extraction.py
+++ tests/extraction.py
@@ -1,40 +1,53 @@
 import sparknlp
 import unittest
+import os
 
-from ..brand_sentiment.extraction import ArticleExtraction
+# # Relative import
+# from ..brand_sentiment.extraction import ArticleExtraction
 
-# spark = sparknlp.start()
-# article_extractor = ArticleExtraction()
-# article = article_extractor.import_one_article('data/article.txt')
-# print(article)
-# sentences = article_extractor.article_to_sentences(article)
-# print(sentences)
+import sys
+sys.path.append('F:\IMPERIAL MATERIAL\4. Group Project\prod-sentiment-package')
+import extraction
 
 
 class TestArticleExtraction(unittest.TestCase):
-
     def __init__(self, *args, **kwargs):
         """ Include useful things for all test cases - keep minimal
         """
         super().__init__(*args, **kwargs)
-
         self.spark = sparknlp.start()
         self.extractor = ArticleExtraction()
 
-    def test_importing_one_article(self):
-        """ Tests are self contained.
-            i.e. only test one function at a time,
-                 and include all prerequisite functions.
-        """
-        article = self.extractor.import_one_article('data/article.txt')
 
+    def test_importing_one_article(self, filepath):
+        """ Check that an article is imported.
+        """
+        article = self.extractor.import_one_article(filepath)
         self.assertIsNotNone(article)
 
-    def test_loading_article_sentences(self):
-        article = self.extractor.import_one_article('data/article.txt')
-        sentences = self.extractor.article_to_sentences(article)
 
+    def test_loading_article_sentences(self, filepath):
+        """ Check that at least one sentence is extracted from an article body text.
+        """
+        article = self.extractor.import_one_article(filepath)
+        sentences = self.extractor.article_to_sentences(article)
         self.assertNotEqual(0, len(sentences))
+
+
+    def test_loading_importing_one_headlines(self, filepath):
+        """ Check that at a headline is extracted from an article.
+        """        
+        headline = self.extractor.import_one_headline_json(filepath)
+        self.assertIsNotNone(headline)
+
+
+    def test_import_folder_headlines(self, folderpath):
+        """ Check that the number of extracted headlines from the folder
+        matches the number of files in it.
+        """
+        headlines = import_folder_headlines(folderpath)
+        num_files = len([1 for x in list(os.scandir(folderpath)) if x.is_file()])
+        self.assertEqual(len(headlines), num_files)
 
 
 if __name__ == "__main__":
