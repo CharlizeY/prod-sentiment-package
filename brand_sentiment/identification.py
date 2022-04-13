f126c09205f43c780c24cd35f6e3b4c6bf98b6df
USER <user@dyn3184-168.wlan.ic.ac.uk>
2022-04-13 17:37:45 +0100

    flake

--- brand_sentiment/identification.py
+++ brand_sentiment/identification.py
@@ -12,44 +12,42 @@ from sparknlp.base import *
 
 # The spark udf function that has to be defined outside the class
 def get_brand(row_list):
-    if not row_list: # If the list is empty
+    if not row_list:  # If the list is empty
         return "None"
-
     else:
         # Create a pandas df with entity names and types
         data = [[row.result, row.metadata['entity']] for row in row_list]
-        df_pd = pd.DataFrame(data, columns = ['Entity', 'Type'])
-      
+        df_pd = pd.DataFrame(data, columns=['Entity', 'Type'])
+
         # Filter only ORGs
         df_pd = df_pd[df_pd["Type"] == "ORG"]
 
         # Rank the ORGs by frequencies
-        ranked_df = df_pd["Entity"].value_counts() # a Pandas Series object
-            
+        ranked_df = df_pd["Entity"].value_counts()  # a Pandas Series object
+
         # If no ORG identified in headline, return None
         if len(ranked_df.index) == 0:
-           return "None"
+            return "None"
 
         # If only one ORG appears in headline, return it
         elif len(ranked_df.index) == 1:
-           return ranked_df.index[0]
+            return ranked_df.index[0]
 
-        # If one ORG appear more than the others, return that one 
+        # If one ORG appear more than the others, return that one
         elif ranked_df[0] > ranked_df[1]:
-            return ranked_df.index[0] 
+            return ranked_df.index[0]
 
-        else: # If multiple ORGs appear the same time, return randomly (TO BE MODIFIED)
+        else:  # If multiple ORGs appear the same time, return randomly (TO BE MODIFIED)
             return random.choice([ranked_df.index[0], ranked_df.index[1]])
             # TO DO: break even - Wikidata for article body #
 
 
-            
 class BrandIdentification:
     def __init__(self, spark_session, model="ner_dl_bert"):
         self.spark = spark_session
         self.model = model
 
-        # Define Spark NLP pipeline 
+        # Define Spark NLP pipeline
         documentAssembler = DocumentAssembler() \
             .setInputCol('text') \
             .setOutputCol('document')
@@ -79,30 +77,34 @@ class BrandIdentification:
             .setOutputCol('ner_chunk')
 
         nlp_pipeline = Pipeline(stages=[
-            documentAssembler, 
+            documentAssembler,
             tokenizer,
             embeddings,
             ner_model,
             ner_converter
         ])
-        
-        # Create the pipeline model
-        empty_df = self.spark.createDataFrame([['']]).toDF('text') # An empty df with column name "text"
-        self.pipeline_model = nlp_pipeline.fit(empty_df)
 
+        # Create the pipeline model
+        empty_df = self.spark.createDataFrame([['']]).toDF('text')  # An empty df with column name "text"
+        self.pipeline_model = nlp_pipeline.fit(empty_df)
 
     def predict_brand(self, text):
         # text could be a pandas dataframe or a Spark dataframe (both with a column "text"), a list of strings or a single string
         # Run the pipeline for the text
-        if isinstance(text, pd.DataFrame): text_df = self.spark.createDataFrame(text) # If input a pandas dataframe
-        elif isinstance(text, list): text_df = self.spark.createDataFrame(pd.DataFrame({'text': text})) # If input a list of strings
-        elif isinstance(text, str): text_df = self.spark.createDataFrame(pd.DataFrame({'text': text}, index=[0])) # If input a single string
-        else: text_df = text
+        if isinstance(text, pd.DataFrame):  # If input a pandas dataframe
+            text_df = self.spark.createDataFrame(text)
+        # AWS EXTRACTION CLASS SHOULD ONLY FEED SPARKDF AS INPUT, HENCE CAN REMOVE BELOW.
+        elif isinstance(text, list):  # If input a list of strings
+            text_df = self.spark.createDataFrame(pd.DataFrame({'text': text}))
+        elif isinstance(text, str):  # If input a single string
+            text_df = self.spark.createDataFrame(pd.DataFrame({'text': text}, index=[0]))
+        else:
+            text_df = text
 
-        df_spark = self.pipeline_model.transform(text_df) 
+        df_spark = self.pipeline_model.transform(text_df)
 
         # Improve speed of identification using Spark User-defined function
-        pred_brand = F.udf(lambda z: get_brand(z), StringType()) # Output a string
+        pred_brand = F.udf(lambda z: get_brand(z), StringType())  # Output a string
         # spark.udf.register("pred_brand", pred_brand)
 
         df_spark_combined = df_spark.withColumn('Predicted_Brand', pred_brand('ner_chunk'))
@@ -110,7 +112,7 @@ class BrandIdentification:
         # df_spark_combined.show(100)
 
         # Remove all rows with no brands detected
-        df_spark_final=df_spark_combined.filter(df_spark_combined.Predicted_Brand != 'None')
+        df_spark_final = df_spark_combined.filter(df_spark_combined.Predicted_Brand != 'None')
         df_spark_final.show(100)
 
         return df_spark_final
--- main.py
+++ main.py
@@ -9,15 +9,17 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.WARN)
 
     spark = sparknlp.start()
-    
+
     logging.warning(f"Running Apache Spark version {spark.version}")
     logging.warning(f"Running Spark NLP version {sparknlp.version()}")
 
     article_extractor = ArticleExtraction()
     brand_identifier = BrandIdentification("ner_dl_bert")
-    #sentimentiser = SentimentIdentification("classifierdl_bertwiki_finance_sentiment_pipeline")
+    sentimentiser = SentimentIdentification("classifierdl_bertwiki_finance_sentiment_pipeline")
 
-    #list_of_headlines = list_of_headlines = article_extractor.import_folder_headlines('articles/https_www_bbc_co_uk_news_technology_60126012')
-    
-    #brand_spark_df = brand_identifier.predict_brand(list_of_headlines)
-    #complete_spark_df = sentimentiser.predict_dataframe(brand_spark_df)
+    headlines = article_extractor.import_folder_headlines('articles/https_www_bbc_co_uk_news_technology_60126012')
+    print(headlines)
+    brand_spark_df = brand_identifier.predict_brand(headlines)
+    print(brand_spark_df)
+    complete_spark_df = sentimentiser.predict_dataframe(brand_spark_df)
+    print(complete_spark_df)
