85173fc9eb2da1d9c223984922e9586ebfa3d3ac
USER <user@dyn3182-218.wlan.ic.ac.uk>
2022-04-29 16:37:49 +0100

    test

--- tmp/csvtoparquet.py
+++ tmp/csvtoparquet.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from langdetect import detect
 
-df = pd.read_parquet('25.parquet')
+df = pd.read_parquet('finetune.parquet')[:2000]
 
 # for i, t in enumerate(df['text'].values):
 #     if detect(t) == 'en':
@@ -10,4 +10,4 @@ df = pd.read_parquet('25.parquet')
 #         df = df.drop(labels=i, axis=0)
 
 # print(df.count())
-df.to_csv('25.csv')
+df.to_csv('finetune.csv')
